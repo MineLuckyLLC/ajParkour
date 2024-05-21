59467b54b0dc374518cc3f4f59e98fcd01f1cbf0
Calvin <youngexplicitpvp@gmail.com>
2024-05-21 17:27:25 -0500

    Update publishing in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,35 +5,35 @@ plugins {
 }
 
 group = "us.ajg0702"
-version = "2.12.10"
+version = "2.12.10-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 
-  maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-  maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
-  maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
-  maven { url = uri("https://maven.enginehub.org/repo/") }
-  maven { url = uri("https://gitlab.com/api/v4/projects/19978391/packages/maven") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
+    maven { url = uri("https://maven.enginehub.org/repo/") }
+    maven { url = uri("https://gitlab.com/api/v4/projects/19978391/packages/maven") }
 }
 
 dependencies {
-  compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
-  compileOnly("me.clip:placeholderapi:2.10.4")
-  compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.0.1")
-  compileOnly(files("libs/InfiniteJump.jar"))
+    compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.10.4")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.0.1")
+    compileOnly(files("libs/InfiniteJump.jar"))
 
-  implementation("com.zaxxer:HikariCP:3.4.5")
-  implementation("org.slf4j:slf4j-simple:1.6.4")
-  implementation("us.ajg0702:ajUtils:1.0.0")
+    implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("org.slf4j:slf4j-simple:1.6.4")
+    implementation("us.ajg0702:ajUtils:1.0.0")
 }
 
 tasks.withType<ProcessResources> {
     include("**/*.yml")
     filter<org.apache.tools.ant.filters.ReplaceTokens>(
-            "tokens" to mapOf(
-                    "VERSION" to project.version.toString()
-            )
+        "tokens" to mapOf(
+            "VERSION" to project.version.toString()
+        )
     )
 }
 
@@ -57,18 +57,19 @@ publishing {
     }
 
     repositories {
+        maven {
+            name = "minelucky-snapshots"
+            url = uri("https://nexus.minelucky.net/repository/maven-snapshots/")
+            credentials {
+                val username = System.getenv("MINELUCKY_REPO_USERNAME")
+                val password = System.getenv("MINELUCKY_REPO_PASSWORD")
 
-        val mavenUrl = "https://repo.ajg0702.us/releases"
-
-        if(!System.getenv("REPO_TOKEN").isNullOrEmpty()) {
-            maven {
-                url = uri(mavenUrl)
-                name = "ajRepo"
-
-                credentials {
-                    username = "plugins"
-                    password = System.getenv("REPO_TOKEN")
+                if (username == null || password == null) {
+                    println("MINELUCKY_REPO_USERNAME or MINELUCKY_REPO_PASSWORD environment variables are not set.")
                 }
+
+                this.username = username
+                this.password = password
             }
         }
     }
